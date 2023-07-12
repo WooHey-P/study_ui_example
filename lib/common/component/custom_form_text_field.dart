ca6865a5fedc7c33eef0c158b508ed81155c75df
WooHyun-P <dkqnwlnd@naver.com>
2023-07-12 22:41:54 +0900

    common custom text form 설정

--- lib/common/component/custom_form_text_field.dart
+++ lib/common/component/custom_form_text_field.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
 
+import '../../colors.dart';
+
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autofocus = false,
+    required this.onChanged,
+    Key? key
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
+    // UnderlineInputBorder: 밑줄
+    // OutlineInputBorder: 외곽선
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1,
+      ),
+    );
 
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      // 비밀번호 입력 시 텍스트 숨김
+      obscureText: obscureText,
+      // 자동 포커스
+      autofocus: autofocus,
+      // 텍스트 변경 시 호출
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        // input 안의 패딩
+        contentPadding: const EdgeInsets.all(20),
+        // 힌트 텍스트
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: const TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14,
+        ),
+        fillColor: INPUT_BG_COLOR,
+        // false: 배경색 적용 안됨
+        // true: 배경색 적용 됨
+        filled: true,
+        // 모든 input 상태의 기본 스타일 적용
+        border: baseBorder,
+        // focus 된 input 상태의 스타일 적용
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          )
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,26 @@ class _App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans'
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CustomTextFormField(),
+            CustomTextFormField(
+              hintText: '이메일을 입력하슈..',
+              autofocus: true,
+              onChanged: (String value) {},
+            ),
+            CustomTextFormField(
+              hintText: '비밀번호를 입력하슈..',
+              obscureText: true,
+              onChanged: (String value) {},
+            ),
           ],
         ),
       ),
